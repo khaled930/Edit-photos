9fc694c236c4fbb5f32a3c9a1e7a9a789df3f206
khaled930 <mnhalmnhal135@gmail.com>
2026-01-05 16:39:50 +0300

    استضافة2

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 from app.routes.image_routes import router as image_router
+import os
+import uvicorn
 
 app = FastAPI(title="Image Editing Platform")
 
@@ -13,3 +15,8 @@ app.include_router(image_router)
 @app.get("/")
 def read_root():
     return RedirectResponse(url="/editor")
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
