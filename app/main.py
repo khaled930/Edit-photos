1af4380670478654bfdef4f2eec3eacdf0e5e5da
khaled930 <mnhalmnhal135@gmail.com>
2026-01-05 12:32:27 +0300

    ً

--- app/main.py
+++ app/main.py
@@ -2,16 +2,10 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 from app.routes.image_routes import router as image_router
-import os
-import uvicorn
-from pathlib import Path
 
 app = FastAPI(title="Image Editing Platform")
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR = BASE_DIR / "static"
-
-app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/uploads", StaticFiles(directory="app/static/uploads"), name="uploads")
 app.include_router(image_router)
 
@@ -19,9 +13,3 @@ app.include_router(image_router)
 @app.get("/")
 def read_root():
     return RedirectResponse(url="/editor")
-
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
